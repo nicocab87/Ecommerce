a7a3fe4eaaafe28a69e3f90838a0a6637c3f7bd4
Nico <nicolas.ferreyram@gmail.com>
2024-01-08 13:26:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+class ProductManager {
+    #id=1;
+
+    constructor(){
+        this.products = []
+    }
+    
+    addProduct(title, description,price,thumbnail,code,stock){
+        let product = {}
+
+        product.title = title;
+        product.description = description;
+        product.price = price;
+        product.thumbnail = thumbnail;
+        product.code = code;
+        product.stock = stock;
+        product.id = this.#id++
+
+        title && description && price && thumbnail && code && stock && (!this.products.some((producto)=> code === producto.code)) ? this.products.push(product) : this.products.push(`Falta agregar datos y/o el codigo agregado ya existe`)
+    }
+
+    getProducts () { return this.products }
+
+    getProductById(id){
+        const searchProduct = this.products.find ((product) => id === product.id)
+
+        return (searchProduct ? searchProduct : 'El producto no existe')
+    }
+} 
