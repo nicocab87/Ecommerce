c7a604fefd3dac19bfcb66bc7eb883c79333c101
Nico <nicolas.ferreyram@gmail.com>
2024-01-22 14:08:27 -0300

    ajustes para la entrega

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # desafio1
+Bienvenidos!
+
+El servidor está corriendo en el puerto 8080!
+
+Para poder ver los productos, entra al link http://localhost:8080/products
+
+Si quieres aplicar un filtro de cuantos productos deseas observar utiliza el query http://localhost:8080/products?limit=NumeroLimiteDeProductos
+
+Si quieres ver un producto en especifico puedes usar el params y buscar por el ID del producto http://localhost:8080/products/IdDelProducto
--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const ProductManager = require ("./ProductManager");
 
+// Creación de productos
+
+const nuevoProducto = new ProductManager (`/Productos.JSON`)
+
+nuevoProducto.addProduct('TV', 'Televisor LED 4K', 50000, 'tv.jpg', 'TV001', 10);
+nuevoProducto.addProduct('Laptop', 'Laptop de alta gama', 80000, 'laptop.jpg', 'LP001', 15);
+nuevoProducto.addProduct('Teléfono', 'Smartphone con cámara dual', 15000, 'phone.jpg', 'PH001', 20);
+nuevoProducto.addProduct('Refrigerador', 'Refrigerador de dos puertas', 30000, 'fridge.jpg', 'RF001', 5);
+nuevoProducto.addProduct('Micrófono', 'Micrófono USB de estudio', 1000, 'mic.jpg', 'MC001', 30);
+nuevoProducto.addProduct('Cámara', 'Cámara DSLR de 24MP', 40000, 'camera.jpg', 'CM001', 8);
+nuevoProducto.addProduct('Auriculares', 'Auriculares inalámbricos', 2000, 'headphones.jpg', 'HD001', 25);
+nuevoProducto.addProduct('Impresora', 'Impresora láser a color', 6000, 'printer.jpg', 'PR001', 12);
+nuevoProducto.addProduct('Tablet', 'Tableta con pantalla táctil', 12000, 'tablet.jpg', 'TB001', 18);
+nuevoProducto.addProduct('Altavoces', 'Altavoces Bluetooth', 3000, 'speakers.jpg', 'SP001', 22);
+
+// Fin de creación de productos
+
 const server = express();
 
 server.get(`/`, (req, res)=>{
@@ -26,3 +43,5 @@ server.get(`/products/:pid`, async (req, res) => {
 })
 
 server.listen(8080,()=>console.log(`Se ha levantado el servidor 8080`))
+
+
