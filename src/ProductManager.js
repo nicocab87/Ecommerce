faeac6c692dbb0bd3658916af3c6483ee846a95a
Nico <nicolas.ferreyram@gmail.com>
2024-02-07 17:06:16 -0300

    arreglos

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -32,10 +32,12 @@ class ProductManager {
         product.status = true;
         product.id = this.#id++
 
-        while((!this.products.some((producto)=> this.#id === producto.id))){
-            this.#id++
-        }
 
+
+        while (this.products.some(producto => producto.id === product.id)) {
+            product.id = this.#id++;
+        }
+    
         const validation = title && description && price && code && stock && (!this.products.some((producto)=> code === producto.code))
     
 
